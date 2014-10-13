905a7e220735fab429764364b9ea016d83a5fc01
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-10-13 16:14:36 +0200

    return strings from n-grams
    We need strings anyway, and this is slightly faster (and easier to
    optimize in Cython).

--- semanticizest/tests/test_wikidump_parser.py
+++ semanticizest/tests/test_wikidump_parser.py
@@ -55,9 +55,9 @@ def test_page_statistics():
 
     assert_equal(dict(links), expected_links)
 
-    assert_in(('And', 'now,'), ngrams)
-    assert_not_in(('Wikipedia', 'We'), ngrams)
-    assert_not_in(('find.', 'And'), ngrams)
+    assert_in('And now,', ngrams)
+    assert_not_in('Wikipedia We', ngrams)
+    assert_not_in('find. And', ngrams)
 
 
 def test_parse_dump():
@@ -69,7 +69,7 @@ def test_parse_dump():
     assert_true(isinstance(ngram_count, dict))
 
     # We need a better tokenizer.
-    assert_in(('Heinrich', 'Tessenow,'), ngram_count)
+    assert_in('Heinrich Tessenow,', ngram_count)
     assert_in(('Heinrich Tessenow', 'Heinrich Tessenow'), link_count)
     assert_greater(link_count[('AMX Index', 'Amsterdam Midkap Index')], 0)
 
