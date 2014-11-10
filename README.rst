9b93cc05c2925429cf7862a8f8ca510066ec19b6
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-11-10 14:59:45 +0100

    readme, slightly smarter tokenizer

--- README.rst
+++ README.rst
@@ -5,6 +5,17 @@ Goal of the semanticizest is to have a working, maintainable,
 librarized version of Daan's original semanticizer.
 
 
+Usage
+-----
+
+To train a semanticizer, download a Wikipedia database dump from
+``https://dumps.wikimedia.org/``. Then issue::
+
+    python -m semanticizest.parse_wikidump <dump> <model-filename>
+
+The result will be a semanticizer model (in SQLite 3 format, if you must know).
+
+
 License
 -------
 
--- semanticizest/_wiki_dump_parser.py
+++ semanticizest/_wiki_dump_parser.py
@@ -172,7 +172,7 @@ def page_statistics(page, N, sentence_splitter=None, tokenizer=None):
 
     if N:
         if tokenizer is None:
-            tokenizer = lambda s: s.split()
+            tokenizer = re.compile(r'\w+').findall
         all_ngrams = chain.from_iterable(ngrams(tokenizer(sentence), N)
                                          for sentence in sentences)
         ngram_counts = Counter(all_ngrams)
@@ -237,6 +237,8 @@ def parse_dump(dump, db, N=7, sentence_splitter=None, tokenizer=None,
         link, ngram = page_statistics(page, N=N, tokenizer=tokenizer,
                                       sentence_splitter=sentence_splitter)
 
+        # We don't count the n-grams within the links, but we need them
+        # in the table, so add them with zero count.
         tokens = chain(six.iteritems(ngram),
                        ((anchor, 0) for _, anchor in six.iterkeys(link)))
         tokens = list(tokens)
@@ -247,9 +249,9 @@ def parse_dump(dump, db, N=7, sentence_splitter=None, tokenizer=None,
                          #for ng, count in tokens)) # six.iteritems(ngram)))
         c.executemany('''insert or ignore into ngrams (ngram) values (?)''',
                       ((g,) for g, _ in tokens))
-        c.executemany('''update ngrams set tf = tf + ?
+        c.executemany('''update ngrams set tf = tf + ?, df = df + 1
                          where ngram = ?''',
-                      tokens)
+                      ((count, token) for token, count in tokens))
 
         c.executemany('''insert or ignore into linkstats values
                          ((select id from ngrams where ngram = ?), ?, 0)''',
