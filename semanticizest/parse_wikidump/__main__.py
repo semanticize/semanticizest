77fdd8b9139f581cde838b9942fd4a99d5493d89
Isaac Sijaranamual <isaacsijaranamual@gmail.com>
2014-11-11 16:33:05 +0100

    Adds --ngram command line option

--- semanticizest/parse_wikidump/__main__.py
+++ semanticizest/parse_wikidump/__main__.py
@@ -1,23 +1,38 @@
+"""parse_wikidump
+
+Usage:
+    parse_wikidump [options] <dump> <model-filename>
+
+Options:
+    --ngram=order, -N=order   Maximum order of the ngrams to extract [default: 7]
+    --help, -h                This help
+"""
 from __future__ import print_function
 
 from os.path import dirname, join
 import sqlite3
 import sys
 
+from docopt import docopt
 from .._wiki_dump_parser import parse_dump
 
 
-if len(sys.argv) != 3:
-    print('usage: %s dump model.db' % sys.argv[0], file=sys.stderr)
-    sys.exit(1)
+def main():
+    args = docopt(__doc__)
+
+    wikidump = args['<dump>']
+    model_fname = args['<model-filename>']
+    ngram = int(args['--ngram'])
+
+    db = sqlite3.connect(model_fname)
+    with open(join(dirname(__file__), "createtables.sql")) as f:
+        create = f.read()
+    c = db.cursor()
+    c.executescript(create)
+    c.execute('pragma synchronous = off')
+
+    parse_dump(wikidump, db, N=ngram, verbose=True)
 
 
-db = sqlite3.connect(sys.argv[2])
-with open(join(dirname(__file__), "createtables.sql")) as f:
-    create = f.read()
-c = db.cursor()
-c.executescript(create)
-
-c.execute('pragma synchronous = off')
-
-parse_dump(sys.argv[1], db, verbose=True)
+if __name__ == '__main__':
+    main()
