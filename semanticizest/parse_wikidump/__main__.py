6c8c79410e7c91647ae72cd608bbe4f684db5125
Daan Odijk <d.odijk@uva.nl>
2014-11-27 10:51:30 +0100

    Implemented --download option for parse_wikidump.

--- semanticizest/parse_wikidump/__main__.py
+++ semanticizest/parse_wikidump/__main__.py
@@ -2,6 +2,7 @@
 
 Usage:
     parse_wikidump [options] <dump> <model-filename>
+    parse_wikidump [options] --download=<wikiname> <dump> <model-filename>
 
 Options:
     --ngram=order, -N order Maximum order of ngrams [default 7, None to disable]
@@ -12,12 +13,21 @@ from __future__ import print_function
 from os.path import dirname, join
 import sqlite3
 
+from six.moves.urllib.request import urlopen
+
 from docopt import docopt
 from .._wiki_dump_parser import parse_dump
 
 
 def main(args):
     wikidump = args['<dump>']
+    if args["--download"]:
+        url = "https://dumps.wikimedia.org/{0}/latest/{0}-latest-pages-articles.xml.bz2".format(args["--download"])
+        dumpstream = urlopen(url) # Can give a HTTPError in case of 404
+        # Probably want to print or log something here
+        with open(wikidump, "w") as out:
+            out.write(dumpstream.read())
+
     model_fname = args['<model-filename>']
     ngram = args['--ngram']
     if ngram not in (None, "None"):
